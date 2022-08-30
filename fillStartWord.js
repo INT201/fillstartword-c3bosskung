638f246f0d71f2f51ef8cff80c57ae71025cd20d
Kira <c3bosskung@gmail.com>
2022-08-30 13:17:35 +0700

    send exam

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,13 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  return `${startWord}${word}`
+  if (startWord === null || word === null || startWord === undefined || word === undefined) {
+    return undefined;
+  } else if (word.startsWith(startWord)) {
+    return `${startWord}${word.replace(startWord, "")}`
+  } else {
+    return `${startWord}${word}`
+  }
 }
+
 module.exports = fillStartWord
